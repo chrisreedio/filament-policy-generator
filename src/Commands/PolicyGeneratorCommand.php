412ea1db87c06a36307c8f2641ccd75e2759333c
Chris Reed <chris@reedtech.us>
2023-11-01 15:03:16 -0500

    Not overwriting policies by default

--- src/Commands/PolicyGeneratorCommand.php
+++ src/Commands/PolicyGeneratorCommand.php
@@ -7,13 +7,14 @@ use Illuminate\Console\Command;
 
 class PolicyGeneratorCommand extends Command
 {
-    public $signature = 'policies:generate';
+    public $signature = 'policies:generate {--overwrite}';
 
     public $description = 'Generate policies for all Filament resources';
 
     public function handle(): int
     {
-        PolicyGenerator::generateAll(true);
+        $overwrite = $this->option('overwrite');
+        PolicyGenerator::generateAll($overwrite);
 
         $this->comment('All policies generated!');
 
