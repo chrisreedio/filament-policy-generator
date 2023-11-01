9e13a103c07b279b4e286338b9d236fca69b214a
Chris Reed <chris@reedtech.us>
2023-11-01 05:21:42 -0500

    Added userpolicy for special handling

--- src/Commands/PolicyGeneratorCommand.php
+++ src/Commands/PolicyGeneratorCommand.php
@@ -21,22 +21,24 @@ class PolicyGeneratorCommand extends Command
 
             $modelName = class_basename($model);
             $policyName = $modelName . 'Policy';
-            $stubFile = __DIR__ . '/stubs/GenericPolicy.stub';
+            $stubDir = __DIR__ . '/stubs/';
+            $stubFile = $stubDir . ($modelName === 'User' ? 'User' : 'Generic') . 'Policy.stub';
             $destPath = base_path('app/Policies/');
             $this->info("Generating {$policyName}...");
+            $replacements = [
+                'Namespace' => config('policy-generator.namespace', 'App'),
+                'UserModelImport' => 'use ' . config('policy-generator.user_model', 'App\Models\User') . ';',
+                'PolicyModelImport' => $modelName === 'User' ? '' : 'use ' . $model . ';',
+                'Model' => $modelName,
+                'modelVariable' => lcfirst($modelName),
+            ];
             StubGenerator::from($stubFile, true) // the stub file path
                 ->to($destPath, true, true) // the store directory path
                 ->as($policyName) // the generatable file name without extension
                 // ->ext('php') // the file extension(optional, by default to php)
                 // ->noExt() // to remove the extension from the file name for the generated file like .env
                 ->replace(true) // to replace the file if already exist // TODO - Check if it exists and ask to overwrite
-                ->withReplacers([
-                    'Namespace' => config('policy-generator.namespace', 'App'),
-                    'UserModelImport' => 'use ' . config('policy-generator.user_model', 'App\Models\User') . ';',
-                    'PolicyModelImport' => $modelName === 'User' ? '' : 'use ' . $model . ';',
-                    'Model' => $modelName,
-                    'modelVariable' => lcfirst($modelName),
-                ]) // the stub replacing params
+                ->withReplacers($replacements) // the stub replacing params
                 ->save(); // save the file
         }
 
